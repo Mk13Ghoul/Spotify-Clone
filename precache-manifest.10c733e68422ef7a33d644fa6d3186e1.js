eaf9e9da0bd600968d4d0baeae4546fafde32dbc
Mk13Ghoul <mayankkumarj5@gmail.com>
2022-03-05 22:17:51 +0530

    Updates

---
+++ precache-manifest.10c733e68422ef7a33d644fa6d3186e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11f24fa2a9c3703654505fec7ef4dbd4",
+    "url": "/Spotify-Clone/index.html"
+  },
+  {
+    "revision": "251d994211659222c802",
+    "url": "/Spotify-Clone/static/css/main.fb7fe8de.chunk.css"
+  },
+  {
+    "revision": "a9b42384bcb10280b5c0",
+    "url": "/Spotify-Clone/static/js/2.92851c38.chunk.js"
+  },
+  {
+    "revision": "277f9ac65ea629533c781a2e9dd27b69",
+    "url": "/Spotify-Clone/static/js/2.92851c38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "251d994211659222c802",
+    "url": "/Spotify-Clone/static/js/main.aea18e02.chunk.js"
+  },
+  {
+    "revision": "50167521508b16e296ab",
+    "url": "/Spotify-Clone/static/js/runtime-main.628ace19.js"
+  }
+]);
